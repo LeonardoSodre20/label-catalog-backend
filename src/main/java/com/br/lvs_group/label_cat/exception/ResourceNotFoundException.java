c9d606d1aad102997a09d21d7e14568099e01f5b
Leonardo Sodré <81453031+LeonardoSodre20@users.noreply.github.com>
2026-05-29 17:50:14 -0300

    feat: add global exception handling

---
+++ src/main/java/com/br/lvs_group/label_cat/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.br.lvs_group.label_cat.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
